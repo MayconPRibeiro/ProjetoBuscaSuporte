0cbec73dc2c64e6a5e734d43b795daad554be4e0
Maycon Ribeiro <maycon_ribeiro10@hotmail.com>
2025-03-14 16:18:23 -0300

    teste sql

---
+++ pastaGabriel/teste.sql
@@ -0,0 +1,32 @@
+create database teste;
+use teste;
+
+
+CREATE TABLE `chamados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(200) NOT NULL,
+  `categoria` varchar(200) NOT NULL,
+  `conteudo` text NOT NULL,
+  `funcionario` varchar(20) DEFAULT NULL,
+  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=809;
+
+
+CREATE TABLE `usuarios` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2;
+
+
+
+use chamados;
+select * from categoria;
+select * from chamado;
+select * from chamados;
+select * from usuario;
+
+
