40282e23130e47dc725f4d1ffe1431c3c479f850
MayconPRibeiro <maycon_ribeiro10@hotmail.com>
2025-03-16 00:32:16 -0300

    Atualizado com conexão ao MySQL

--- testes.py
+++ testes.py
@@ -3,19 +3,22 @@
 dados = consultar()
 
 print(dados)'''
-
-
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
+load_dotenv()
 connection = None  # Define a variável connection antes do bloco try
 
 try:
-    connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='FitAirlines'
-    )
+    db_config = {
+        'host' : os.getenv('DB_HOST'),
+        'user' : os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'database': os.getenv('DB_NAME')
+    }
+
+    connection = mysql.connector.connect(**db_config)
     print("Conexão bem-sucedida")
 except mysql.connector.Error as err:
     print(f"Erro: {err}")
