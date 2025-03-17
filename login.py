062b5888c01d86880abee16b52e2e2b5561ab836
MayconPRibeiro <maycon_ribeiro10@hotmail.com>
2025-03-17 19:25:13 -0300

    Adicionado redirecionamento de paginas conforme role

--- login.py
+++ login.py
@@ -1,9 +1,27 @@
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import conexao_db
 from flask import Flask, render_template, redirect, url_for, request, flash
-from werkzeug import generate_password_hash, check_password_hash
 from main import app
+from conexao_db import conectar
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "index"
+
+class User(UserMixin):
+    def __init__(self, id, email, role):
+        self.id = id
+        self.email = email
+        self.role = role
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = conectar()
+    cursor = conn.cursor(dictionary = True)
+    cursor.execute('SELECT * FROM usuarios WHERE id = %s', (user_id))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if user:
+        return User(user['id'], user['email'], user['role'])
+    return None
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
-from flask import redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug import generate_password_hash, check_password_hash
 from conexao_db import conectar
+from login import current_user, User, login_manager
 
 
 app = Flask(__name__) #objeto flask
@@ -10,11 +12,47 @@ app.secret_key = 'ASDB1578963' #chave secreta
 @app.route("/", methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
 
+        conn = conectar()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if user and check_password_hash(user['password'], password):
+            user_obj = User(id=user['id'], email=user['email'], role=user['role'])
+            login_user(user_obj)
+            flash("Seja Bem Vindo(a)!", "sucesso")
+
+        if user['role'] == 'gestor':
+            return redirect(url_for('pagina_inicial'))
+        elif user['role'] == 'tecnico':
+            return redirect(url_for('pagina_inicial_tecnico'))
+        elif user['role'] == 'cliente':
+            return redirect(url_for('pagina_inicial_cliente'))
+        
+        else:
+            flash('Login ou Senha inválido', 'sem sucesso')
     #return render_template("index.html")
     return render_template("pagina_inicial.html")
+
+
+@app.route("/pagina_inicial_tecnico")
+@login_required
+def pagina_inicial_tecnico():
+    if current_user.role == "tecnico":
+        return render_template("pagina_inicial_tecnico.html")
+    else:
+        return redirect(url_for("index"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route("/pagina_inicial", methods=['POST', 'GET'])
 def pagina_inicial():
@@ -66,8 +104,6 @@ def sugestao():
         msg = f"Erro ao enviar sugestão: {str(erro)}"
 
     return render_template("sugestao.html", msg = 'Enviado com sucesso!')
-        
-    
 
 
 if __name__ == "__main__":
